e4fa351aa03b222a4f10e83e6c2cf984195eb8d7
rahul110 <rahul@transpacks.co>
2020-04-22 12:08:38 +0530

    adding next page in order details

--- src/app/customer/storedetail/storedetail.component.ts
+++ src/app/customer/storedetail/storedetail.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnChanges } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Utils } from 'src/app/shared/utils';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-storedetail',
@@ -9,11 +10,42 @@ import { Utils } from 'src/app/shared/utils';
 })
 export class StoredetailComponent implements OnInit {
 
-  constructor(private titleService: Title) {
+  fileName:string = 'Upload Order Image';
+  @ViewChild('orderText',{static: true}) orderText: ElementRef;
+
+  constructor(private titleService: Title, private router:Router, private activatedRoute: ActivatedRoute ) {
     this.titleService.setTitle('Store Detail' + Utils.getAppName());
   }
 
   ngOnInit(): void {
+    // this.router.navigate(['']);
+  }
+
+  fileProgress(fileInput: any) {
+    // let file = fileInput.targe
+  }
+
+  onChange(file: HTMLInputElement) {
+    let filePathArray = file.value.split('\\');
+    let fileName = filePathArray[filePathArray.length-1];
+    this.fileName = fileName
+  }
+
+  orderExists() {
+    if(this.orderText.nativeElement.value!=='' || (this.fileName!=='Upload Order Image' && this.fileName!=''))
+    return true;
+    else
+    return false;
+  }
+
+  onNextPage() {
+    // console.log(this.activatedRoute)
+    console.log("in next page");
+    console.log(this.router);
+    this.router.navigate(['customer/userform']);
+  }
+  // Dont remove the below function checks for any keypresses in text feild
+  onTextEnter(orderText: HTMLInputElement) {
   }
 
 }
